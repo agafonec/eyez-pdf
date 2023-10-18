63e2fd0f2331b137402a17b0f9b35cf3f35aa0bb
DANYLO <agafonec1@gmail.com>
2023-10-18 12:52:23 +0300

    Initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="text-3xl font-medium">{{ title }}</div>
+</template>
+
+<script>
+export default {
+    props: {
+        title: {
+            type: String
+        }
+    },
+    name: "Home"
+}
+</script>
+
+<style scoped>
+
+</style>
