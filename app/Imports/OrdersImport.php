8b931eccdd6e18828e2403c662e9ead718bf267e
DANYLO <agafonec1@gmail.com>
2023-12-18 19:37:08 +0200

    Added order_time support for importing orders from csv.

--- app/Imports/OrdersImport.php
+++ app/Imports/OrdersImport.php
@@ -29,8 +29,14 @@ class OrdersImport implements ToModel, WithHeadingRow, WithBatchInserts
             'order_id'     => $row['order_id']
         ])->first();
         if (!$order) {
-            $dateString = str_contains($row['order_date'], '/') ? str_replace('/', '-', $row['order_date']) : $row['order_date'] ;
-            $datetime = new \DateTime($dateString, new \DateTimeZone('Asia/Jerusalem'));
+            if (isset($row['order_time'])) {
+                $dateString = str_contains($row['order_date'], '-') ? str_replace('-', '/', $row['order_date']) : $row['order_date'];
+                $dateString = "{$dateString} {$row['order_time']}";
+                $datetime = new \DateTime($dateString, new \DateTimeZone('Asia/Jerusalem'));
+            } else {
+                $dateString = str_contains($row['order_date'], '/') ? str_replace('/', '-', $row['order_date']) : $row['order_date'];
+                $datetime = new \DateTime($dateString, new \DateTimeZone('Asia/Jerusalem'));
+            }
             return new Order([
                 'store_id' => $this->storeId,
                 'order_id'    => $row['order_id'],
