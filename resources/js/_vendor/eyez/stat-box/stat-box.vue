8cf237b4d1ad0f3b5025d30558f96ffcaa3ec804
DANYLO <agafonec1@gmail.com>
2024-02-12 11:25:22 +0200

    Build with arrow fix.

--- resources/js/_vendor/eyez/stat-box/stat-box.vue
+++ resources/js/_vendor/eyez/stat-box/stat-box.vue
@@ -1,8 +1,8 @@
 <template>
     <div v-if="variant === 'small'" :class="['flex items-center md:flex-row w-full relative', `${mobileDirection === 'row' ? 'flex-row': 'max-md:flex-col max-md:text-center'}`]">
-        <div v-if="showDifference" :class="['text-sm md:text-base absolute end-0 top-1 flex items-center', `${stat.current.value >= stat.previous.value ? 'text-green-300' : 'text-red-300'}`]">
+        <div v-if="showDifference" :class="['text-sm md:text-base absolute end-0 top-1 flex items-center', `${stat.current.value >= parseInt(stat.previous.value) ? 'text-green-300' : 'text-red-300'}`]">
             <span class="font-medium leading-none">{{ percent }}</span>
-            <icon-arrow-up v-if="stat.current.value >= stat.previous.value" calss="text-green-300"/>
+            <icon-arrow-up v-if="stat.current.value >= parseInt(stat.previous.value)" calss="text-green-300"/>
             <icon-arrow-down v-else class="text-red-300"/>
         </div>
 
@@ -30,9 +30,9 @@
                 <div class="text-sm text-gray-300 leading-tight">{{ stat.current.title }}</div>
                 <div class="text-base md:text-2xl text-gray-900 leading-tight flex">
                     <div>{{ stat.current.value.toLocaleString() }}{{ appendToValue }}</div>
-                    <div v-if="showDifference" :class="['text-sm md:text-base mr-2 flex items-center', `${stat.current.value >= stat.previous.value ? 'text-green-300' : 'text-red-300'}`]">
+                    <div v-if="showDifference" :class="['text-sm md:text-base mr-2 flex items-center', `${stat.current.value >= parseInt(stat.previous.value) ? 'text-green-300' : 'text-red-300'}`]">
                         <span class="font-medium leading-none">{{ percent }}</span>
-                        <icon-arrow-up v-if="stat.current.value >= stat.previous.value" calss="text-green-300"/>
+                        <icon-arrow-up v-if="stat.current.value >= parseInt(stat.previous.value)" calss="text-green-300"/>
                         <icon-arrow-down v-else class="text-red-300"/>
                     </div>
                 </div>
